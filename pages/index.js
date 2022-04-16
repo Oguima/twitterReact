06d4c1e14fd6c366a60f141e8866b14ec0b74b85
Rafael Guimarães dos Santos <rafaelguima@outlook.com>
2022-04-16 14:41:08 -0300

    contador exemplo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+
 function Home() {
-    return <div>Home teste</div>
+    return (
+        <div> 
+            <div>Home</div>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1); //atualzaContador, setContador convenção... 
+
+    //let contador = 10;
+
+    function adicionarContador() {
+        setContador(contador + 1);
+        //contador = contador + 1;
+        //console.log('Adicionou: ' + contador);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
 }
 
 export default Home 
